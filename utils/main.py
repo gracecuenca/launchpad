744df83f9a112c930a30f611ab8e3589246ce3d3
gracecuenca <gcuenca@stuy.edu>
2018-02-20 22:19:21 -0500

    auth

--- utils/main.py
+++ utils/main.py
@@ -1,7 +1,37 @@
 import sys
 import json
 import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy.util as util
 
 spotify = spotipy.Spotify()
 
-#dependencies: uri of artists
+#dependencies: track of song
+#test: Accidentally in Love by Counting Crows
+#spotify:track:7FQSD5JjWqGtS1BaQQiT6V
+
+scope = 'user-library-modify'
+
+if len(sys.argv) > 2:
+    username = sys.argv[1]
+    tids = sys.argv[2:]
+else:
+    print("Usage: %s username track-id ..." % (sys.argv[0],))
+    sys.exit()
+
+token = util.prompt_for_user_token(username, scope)
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+    sp.trace = False
+    results = sp.current_user_saved_tracks_add(tracks=tids)
+    pprint.pprint(results)
+else:
+    print("Can't get token for", username)
+
+client_credentials_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+song_id = 'spotify:track:7FQSD5JjWqGtS1BaQQiT6V'
+analysis = sp.audio_analysis(song_id)
+print(json.dumps(analysis, indent=4))
