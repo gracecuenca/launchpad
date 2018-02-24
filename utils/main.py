59d9160fc4afe9315c416bf3dd31ed05361d1a59
gracecuenca <gcuenca@stuy.edu>
2018-02-23 21:27:17 -0500

    test

--- utils/main.py
+++ utils/main.py
@@ -35,3 +35,28 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 song_id = 'spotify:track:7FQSD5JjWqGtS1BaQQiT6V'
 analysis = sp.audio_analysis(song_id)
 print(json.dumps(analysis, indent=4))
+
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+sp.trace=False
+
+if len(sys.argv) > 1:
+    artist_name = ' '.join(sys.argv[1:])
+else:
+    artist_name = 'weezer'
+
+results = sp.search(q=artist_name, limit=50)
+tids = []
+for i, t in enumerate(results['tracks']['items']):
+    print(' ', i, t['name'])
+    tids.append(t['uri'])
+
+start = time.time()
+features = sp.audio_features(tids)
+delta = time.time() - start
+for feature in features:
+    print(json.dumps(feature, indent=4))
+    print()
+    analysis = sp._get(feature['analysis_url'])
+    print(json.dumps(analysis, indent=4))
+    print()
+print ("features retrieved in %.2f seconds" % (delta,))
